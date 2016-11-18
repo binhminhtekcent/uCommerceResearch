666a304fae5ed4c18d98edbe0a67e2c952b78cbf
Minh Do <minhlbdo@tekcent.com>
2016-11-18 17:39:43 +0700

    Add Controller

---
+++ uCommerceSiteDev/uCommerceSiteDev/Controllers/FeatureItemController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uCommerceSiteDev.Models;
+using UCommerce.Api;
+using UCommerce.Content;
+using UCommerce.EntitiesV2;
+using UCommerce.Infrastructure;
+using UCommerce.Runtime;
+using Umbraco.Web.Mvc;
+
+namespace uCommerceSiteDev.Controllers
+{
+    public class FeatureItemController : SurfaceController
+    {
+        // GET: FeatureItem
+        public ActionResult Index()
+        {
+            IEnumerable<Product> products = SiteContext.Current.CatalogContext.CurrentCatalog.Categories.SelectMany<Category, Product>((Category c) =>
+                from p in c.Products
+                where p.ProductProperties.Any<ProductProperty>((ProductProperty pp) =>
+                {
+                    if (pp.ProductDefinitionField.Name != "ShowOnHomepage")
+                    {
+                        return false;
+                    }
+                    return Convert.ToBoolean(pp.Value);
+                })
+                select p);
+            ProductsViewModel productsViewModel = new ProductsViewModel();
+            foreach (Product product in products)
+            {
+                productsViewModel.Products.Add(new ProductViewModel()
+                {
+                    Name = product.Name,
+                    PriceCalculation = CatalogLibrary.CalculatePrice(product, null),
+                    Url = CatalogLibrary.GetNiceUrlForProduct(product, null, null),
+                    Sku = product.Sku,
+                    IsVariant = product.IsVariant,
+                    VariantSku = product.VariantSku,
+                    ThumbnailImageUrl = ObjectFactory.Instance.Resolve<IImageService>().GetImage(product.ThumbnailImageMediaId).Url
+                });
+            }
+            return View("/Views/FeatureItem.cshtml", productsViewModel);
+        }
+    }
+}
---
+++ uCommerceSiteDev/uCommerceSiteDev/Controllers/NavigationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using uCommerceSiteDev.Models;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
+using UCommerce.Extensions;
+using Umbraco.Web.Mvc;
+namespace uCommerceSiteDev.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public ActionResult Index()
+        {
+            CategoryNavigationViewModel categoryNavigationViewModel = new CategoryNavigationViewModel();
+            //int? nullable = null;
+            //categoryNavigationViewModel.Categories = this.MapCategories(CatalogLibrary.GetRootCategories(nullable));
+            categoryNavigationViewModel.Categories = this.MapCategories(CatalogLibrary.GetRootCategories());
+            return View("/views/Navigation.cshtml", categoryNavigationViewModel);
+        }
+
+        private IList<CategoryViewModel> MapCategories(ICollection<Category> categoriesToMap)
+        {
+            List<CategoryViewModel> categoryViewModels = new List<CategoryViewModel>();
+            foreach (Category category in categoriesToMap)
+            {
+                CategoryViewModel categoryViewModel = new CategoryViewModel()
+                {
+                    Name = CategoryExtensions.DisplayName(category),
+                    Url = CatalogLibrary.GetNiceUrlForCategory(category, null),
+                    Categories = this.MapCategories(CatalogLibrary.GetCategories(category))
+                };
+                categoryViewModels.Add(categoryViewModel);
+            }
+            return categoryViewModels;
+        }
+    }
+}
